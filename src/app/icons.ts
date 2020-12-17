771c3e3561d622ab6541d155cff5d5f5b5fbd47f
Shivank-Mittal <mittal.shivank@gmail.com>
2020-12-17 23:11:55 +0100

    Change landing page and included wave top

--- src/app/icons.ts
+++ src/app/icons.ts
@@ -1,15 +1,15 @@
-export enum icons{
-    Linkedin = "linkedin",
-    Email = "email",
-    instagram = "instagram",
-    facebook= "facebook",
-    github = "github"
+export enum icons {
+  Linkedin = "linkedin",
+  Email = "email",
+  instagram = "instagram",
+  facebook = "facebook",
+  github = "github",
 }
 
-export enum socialAccounts{
-    linkedin = "https://www.linkedin.com/in/shivank-mittal-09055ba3",
-    email = "email",
-    instagram = "https://www.instagram.com/shivank.m/",
-    facebook= "https://www.facebook.com/shivank.mittal.7",
-    github = "https://github.com/Shivank-Mittal"
+export enum socialAccounts {
+  linkedin = "https://www.linkedin.com/in/shivank-mittal-09055ba3",
+  email = "email",
+  instagram = "https://www.instagram.com/shivank.m/",
+  facebook = "https://www.facebook.com/shivank.mittal.7",
+  github = "https://github.com/Shivank-Mittal",
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,44 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { icons, socialAccounts } from '../icons';
+import { Component, OnInit } from "@angular/core";
+import { icons, socialAccounts } from "../icons";
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  selector: "app-main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.css"],
 })
 export class MainComponent implements OnInit {
+  public socialMediaAcountIcons = [
+    icons.Email,
+    icons.Linkedin,
+    icons.github,
+    icons.facebook,
+    icons.instagram,
+  ];
+  public dasboardMenue = ["ABOUT", "WORK", "CONTACT"];
+  constructor() {}
 
+  ngOnInit() {}
 
-  public socialMediaAcountIcons = [icons.Email, icons.Linkedin, icons.github , icons.facebook, icons.instagram];
-  public dasboardMenue = ["ABOUT","WORK","CONTACT"];
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  public goToLink(clickedIcon : string){
-
-    if(clickedIcon != icons.Email){
-
+  public goToLink(clickedIcon: string) {
+    if (clickedIcon != icons.Email) {
       window.open(socialAccounts[clickedIcon]);
-    }
-    else{
-      const selBox = document.createElement('textarea');
+    } else {
+      const selBox = document.createElement("textarea");
       selBox.value = "mittal.shivank@gmail.com";
       document.body.appendChild(selBox);
       selBox.focus();
       selBox.select();
-      document.execCommand('copy');
+      document.execCommand("copy");
       document.body.removeChild(selBox);
-
     }
-}
-  public toLoacation( item : string)
-  {
-    console.log("test");
-    console.log(item)
-    document.getElementById(item).scrollIntoView({behavior:"smooth"});
   }
-
-
+  public toLoacation(item: string) {
+    console.log("test");
+    console.log(item);
+    document.getElementById(item).scrollIntoView({ behavior: "smooth" });
+  }
 }
