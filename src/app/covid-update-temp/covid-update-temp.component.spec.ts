db2ce4b6247655b1b9be660ffb411c742b1efec8
Shivank-Mittal <mittal.shivank@gmail.com>
2020-12-18 00:01:47 +0100

    updated angular material

--- src/app/covid-update-temp/covid-update-temp.component.spec.ts
+++ src/app/covid-update-temp/covid-update-temp.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CovidUpdateTempComponent } from './covid-update-temp.component';
 
@@ -6,7 +6,7 @@ describe('CovidUpdateTempComponent', () => {
   let component: CovidUpdateTempComponent;
   let fixture: ComponentFixture<CovidUpdateTempComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CovidUpdateTempComponent ]
     })
