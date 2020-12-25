c5296ae37d8f03cbcd01c22472078d1daede6747
Shivank-Mittal <mittal.shivank@gmail.com>
2020-12-25 22:09:59 +0100

    created the social icons for footer

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -12,4 +12,29 @@ export class ContactComponent implements OnInit {
 
   }
 
+  onpenSocialPage(page) {
+    const Linkedin_url = "https://www.linkedin.com/in/shivank-mittal-09055ba3/"
+    const Github_url = "https://github.com/Shivank-Mittal"
+    const Facebook_url = "https://www.facebook.com/shivank.mittal.7"
+    const twitter_url = "https://twitter.com/shivankmittal"
+
+    switch (page) {
+      case "facebook":
+        window.open(Facebook_url, '_blank');
+        break;
+      case "twitter":
+        window.open(twitter_url, '_blank');
+        break;
+      case "github":
+        window.open(Github_url, '_blank');
+        break;
+      case "lnkedin":
+        window.open(Linkedin_url, '_blank');
+        break;
+
+      default:
+        break;
+    }
+  }
+
 }
