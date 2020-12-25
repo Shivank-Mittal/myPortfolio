6bdd70fc572f0f559c3392e9b75f19728933566d
Shivank-Mittal <mittal.shivank@gmail.com>
2020-12-25 01:37:54 +0100

    updated the heroku configrations

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/myWebApplicationUI'));
+app.use(express.static('./dist/myWebApplicationUI'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/myWebApplicationUI/index.html'));
+res.sendFile(path.join('./dist/myWebApplicationUI/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
